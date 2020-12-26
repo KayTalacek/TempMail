9dfabb97eb94cb99d2ce34478e3cfb3af7b7bdd3
Lukas Talacek <Lukas.T5@seznam.cz>
2020-12-27 00:42:41 +0100

    Repaired file DataHandler

--- TempMail/TempMail/Data Handler/DataHandler.swift
+++ TempMail/TempMail/Data Handler/DataHandler.swift
@@ -10,25 +10,23 @@ import Foundation
 class DataHandler {
     static let defaults = UserDefaults.standard
 
-<<<<<<< Updated upstream
     static func saveEmail(_ value: String) {
         defaults.set(value, forKey: "temporaryEmail")
-=======
+    }
+
     static func saveEmail(email: String, key: String) {
         defaults.set(email, forKey: "temporaryEmail")
         saveEmailKey(key)
->>>>>>> Stashed changes
     }
+        
     static func getEmail() -> String {
         return defaults.object(forKey: "temporaryEmail") as? String ?? ""
     }
-<<<<<<< Updated upstream
-=======
+
     static func saveEmailKey(_ key: String) {
         defaults.set(key, forKey: "temporaryEmailKey")
     }
     static func getEmailKey() -> String {
         return defaults.object(forKey: "temporaryEmailKey") as? String ?? ""
     }
->>>>>>> Stashed changes
 }
